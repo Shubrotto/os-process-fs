f7ec483bca9cb77d3cff76c54c03e9a7b4088e6d
Shubrotto Kumar Hawlader <skshubroto@gmail.com>
2024-04-03 17:16:41 +0600

    fs file write & read

---
+++ index.js
@@ -0,0 +1,28 @@
+const os = require("os");
+const fs = require("fs");
+
+// console.log(os.cpus());
+// console.log(os.platform());
+// console.log(process.version);
+// console.log(process.cpuUsage());
+
+const data = {
+  os: {
+    "homeDirect" : os.homedir(),
+    "platform": os.platform(),
+    "cpuDetails": os.cpus(),
+  },
+  process: {
+    
+    "version": process.version,
+    "cpuUsage": process.cpuUsage(),
+  }
+}
+fs.writeFileSync(
+  "sys_data.json",
+  JSON.stringify(data)
+);
+
+const file = fs.readFileSync("sys_data.json", { encoding: "utf-8", flag: "r" });
+const fileSystem = JSON.parse(file);
+console.log(fileSystem);
